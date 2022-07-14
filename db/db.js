d5193ba2d7863d5924fee4d9d5b54b8952d753bc
Mayyyyyyyy <8633009+mayyyyyyyy@user.noreply.gitee.com>
2022-07-14 14:30:44 +0800

    hide code

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const dbConfig = {
     host:'localhost',
     user:'root',
-    password:'mysql1996',
+    password:'*****',
     // port:'8080',
     database: 'user',
 }
